1f0efef232bfd91822ba1e7fd3a1e0f41fd44337
Luís Gustavo <group.plays@gmail.com>
2023-08-23 03:57:30 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,31 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photo":"https://i.imgur.com/u6iw3EU.png",
+    "title": "Confira a nova personagem: Acates Horizonte Infinito",
+    "description": "A nova personagem 4 estrelas é uma tece almas de luz com foco em limpar os debuffs do time e transformá-los em buffs, mecanica essa que nunca havia \
+    sido utilizada anteriormente. A ML Acates é do signo de peixes e possui defesa alta para um tece almas, o imprint para o time é Ataque% e o concentrado é Resistencia. \
+    A S1 tem 40% de chance de atordoar, com o Soulburn a chance aumenta para 100%. \
+    A S2 remove dois debuffs de um aliado (exceto dela mesma) e dá barreira por dois turnos antes de aumentar a CR em 40%, a barreira escala com a vida da Acates. \
+    A S3 remove todos os debuffs de todos os aliados e dá um buff aleatório por dois turnos para cada debuff que foi removido e recupera vida. A quantidade de vida recuperada \
+    escala com a vida máxima da Acates. Quando a skill está disponível, no fim do turno de um inimigo, aumenta a CR da Acates em 15%."
+  },
+  {
+    "id":"2",
+    "photo":"https://i.imgur.com/CpH79VT.png",
+    "title": "Quem pegar na rotação do dia 24/08?",
+    "description": "Qualquer um, menos a Aramintha."
+  },
+  {
+    "id":"3",
+    "photo":"https://i.imgur.com/MegtWgI.png",
+    "title": "Rede Crystallis recruta membros competitivos",
+    "description": "Com a season terminando, ambas as guildas competitivas da Rede Crystallis anunciam vagas para quem tiver interesse em competir. Saiba mais ao entrar no discord."
+  },
+  {
+    "id":"4",
+    "photo":"https://i.imgur.com/ARGj5yK.png",
+    "title": "Amethystus mais uma vez fora do top 100",
+    "description": "A guilda mais amada do Brasil finalizou a temporada no rank 113. O líder da guilda afirmou que jogaram como nunca mas perderam como sempre."
+  }
+]
